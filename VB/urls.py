0fac9a9674f657ef620645ad31ab8b5dc4ed4dee
Raj Rawat <raj.rawat@ucalgary.ca>
2025-04-20 00:47:31 -0600

    Completeing the backend requirements for resetting password

--- VB/urls.py
+++ VB/urls.py
@@ -18,6 +18,9 @@ urlpatterns = [
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"), name='password_reset_confirm'),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"), name='password_reset_complete'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"), name='password_reset_done'
+),
+
 ]
 
 # used to store media files during deployment
